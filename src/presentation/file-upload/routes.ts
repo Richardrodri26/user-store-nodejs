99f9bb3b52e91cf3f95b8e59a422dcfa2ca4adc5
RichardRodri26 <richardmanuel811@gmail.com>
2025-09-15 21:31:57 -0500

    feat: implement file upload controller and routes for single and multiple file uploads

---
+++ src/presentation/file-upload/routes.ts
@@ -0,0 +1,36 @@
+import { Router } from 'express';
+
+
+
+
+
+export class FileUploadRoutes {
+
+
+  static get routes(): Router {
+
+    const router = Router();
+    const controller = new FileUploadController(
+      new FileUploadService()
+    );
+
+
+    router.use( FileUploadMiddleware.containFiles );
+    router.use( TypeMiddleware.validTypes(['users','products','categories']) );
+
+    
+    // Definir las rutas
+    // api/upload/single/<user|category|product>/
+    // api/upload/multiple/<user|category|product>/
+    router.post( '/single/:type', controller.uploadFile );
+    router.post( '/multiple/:type',controller.uploadMultileFiles );
+
+
+
+
+    return router;
+  }
+
+
+}
+
